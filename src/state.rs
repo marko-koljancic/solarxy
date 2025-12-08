951fa3bc83f70522dede8344046f9d73cfcd26f1
marko-koljancic <marko.koljancic@gmail.com>
2025-12-08 14:55:48 +0100

    Remove unused instance generation logic and related constants in State

--- src/state.rs
+++ src/state.rs
@@ -8,7 +8,6 @@ use std::sync::Arc;
 use wgpu::util::DeviceExt;
 use winit::{event_loop::ActiveEventLoop, keyboard::KeyCode, window::Window};
 
-const NUM_INSTANCES_PER_ROW: u32 = 1;
 const BACKGROUND_COLOR: wgpu::Color = wgpu::Color {
     r: 0.4235,
     g: 0.4588,
@@ -274,26 +273,6 @@ impl State {
         });
 
         let camera_controller = CameraController::new(0.2);
-
-        // const SPACE_BETWEEN: f32 = 3.0;
-        // let instances = (0..NUM_INSTANCES_PER_ROW)
-        //     .flat_map(|z| {
-        //         (0..NUM_INSTANCES_PER_ROW).map(move |x| {
-        //             let x = SPACE_BETWEEN * (x as f32 - NUM_INSTANCES_PER_ROW as f32 / 2.0);
-        //             let z = SPACE_BETWEEN * (z as f32 - NUM_INSTANCES_PER_ROW as f32 / 2.0);
-
-        //             let position = cgmath::Vector3 { x, y: 0.0, z };
-
-        //             let rotation = if position.is_zero() {
-        //                 cgmath::Quaternion::from_axis_angle(cgmath::Vector3::unit_z(), cgmath::Deg(0.0))
-        //             } else {
-        //                 cgmath::Quaternion::from_axis_angle(position.normalize(), cgmath::Deg(0.0))
-        //             };
-
-        //             Instance { position, rotation }
-        //         })
-        //     })
-        //     .collect::<Vec<_>>();
 
         let instance = Instance {
             position: cgmath::Vector3 { x: 0.0, y: 0.0, z: 0.0 },
