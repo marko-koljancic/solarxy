4f032318877d7c363cb0273c9955a56f6e0f9e28
marko-koljancic <marko.koljancic@gmail.com>
2025-12-10 14:22:22 +0100

    refactor: Clean up imports and remove debug print statements in main function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use clap::Parser;
-use crate::cli::parser::{Args, OperationMode};
+use crate::cli::{
+    parser::{Args, OperationMode},
+    tui::TerminalApp,
+};
 use crate::calc::analyize::ModelAnalyzer;
-use crate::cli::tui::TerminalApp;
 use solarxy::{run_viewer};
 use std::fs;
 
@@ -12,10 +14,6 @@ mod cli;
 
 fn main() -> io::Result<()> {
     let args = Args::parse();
-    println!("::: Solarxy starting :::");
-    println!("Model path >>> {}", args.model_path.display());
-    println!("Operation mode >>> {:?}", args.mode);
-
     let model_path_buff = fs::canonicalize(&args.model_path).expect("Failed to canonicalize the model path");
     let model_path = model_path_buff.to_string_lossy().to_string();
 
